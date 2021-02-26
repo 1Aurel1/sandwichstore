d79cc2b67abaa256e57aa3ceaa492b4468972dfc
Aurel marishta <aurel.marishta@gmail.com>
2021-02-26 15:43:06 +0100

    removed Ingredient

--- sandwich-shop-rest/src/main/java/com/example/sandwichshoprest/sandwich/Sandwich.java
+++ sandwich-shop-rest/src/main/java/com/example/sandwichshoprest/sandwich/Sandwich.java
@@ -3,23 +3,18 @@ package com.example.sandwichshoprest.sandwich;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Data
 @Entity
 public class Sandwich {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     @Enumerated(EnumType.STRING)
     private SandwichType type;
     private Double price;
-
-    @OneToMany(
-            mappedBy = "sandwich",
-            fetch = FetchType.LAZY,
-            orphanRemoval = true
-    )
-    private List<Ingredient> ingredients;
+    private String ingredients;
 }
